3128eb632c456a0e5c3891056a0f57599e64677f
KenMarquez <darius_dam@hotmail.com>
2016-09-20 20:10:55 -0500

    Agregamos el item del recycler view de ejemplo

--- recyclerviewexample/src/main/java/io/mariachi/recyclerviewexample/MainActivity.java
+++ recyclerviewexample/src/main/java/io/mariachi/recyclerviewexample/MainActivity.java
@@ -2,13 +2,21 @@ package io.mariachi.recyclerviewexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerContacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        mRecyclerContacts = (RecyclerView) findViewById(R.id.recycler_contacts);
     }
 
 }
